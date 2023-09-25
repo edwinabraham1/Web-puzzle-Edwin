df7d97c9b886a1459b97e5bf6130449c9f9bd359
edwin nalukurthi <edwinabraham.n@gmail.com>
2023-09-25 17:18:29 -0500

    unsubscribe o bservables,snackback duration

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -11,4 +11,36 @@ describe('When: I use the reading list feature', () => {
       'My Reading List'
     );
   });
+
+  it('undo adding book into reading list', () => {
+
+    // NOTE : CLEAR ALL READING LIST
+
+    cy.get('input[type="search"]').type('javascript');
+    cy.wait(100)
+    cy.get('[data-testing="click-search-icon"]').click();
+    cy.wait(200)
+    cy.get('[data-testing="button-to-make-read-9U5I_tskq9MC"]').should('be.visible').click() // button will be disabled after clicking 
+    cy.wait(100)
+    cy.contains('undo').should("be.visible").click();
+    cy.get('[data-testing="button-to-make-read-9U5I_tskq9MC"]').should('be.visible') // button will be enabled once undo is clicked from Reading List
+
+  });
+
+  it('undo removed book from reading list', () => {
+
+    // NOTE : CLEAR ALL READING LIST
+
+    cy.get('input[type="search"]').type('javascript');
+    cy.wait(100)
+    cy.get('[data-testing="click-search-icon"]').click();
+    cy.wait(200)
+    cy.get('[data-testing="button-to-make-read-9U5I_tskq9MC"]').should('be.visible').click() // button will be disabled after clicking 
+    cy.get('[data-testing="toggle-reading-list"]').click();
+    cy.get('[data-testing="remove-item-from-list"]').click();
+    cy.wait(100)
+    cy.contains('undo').should("be.visible").click();
+    cy.get('[data-testing="button-to-make-read-9U5I_tskq9MC"]').should('be.visible') // button will be enabled once undo is clicked from Reading List
+
+  });
 });
--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -1,28 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import {
   addToReadingList,
   clearSearch,
   getAllBooks,
   ReadingListBook,
+  removeFromReadingList,
   searchBooks
 } from '@tmo/books/data-access';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Book } from '@tmo/shared/models';
+import { Book,ReadingListItem } from '@tmo/shared/models';
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'tmo-book-search',
   templateUrl: './book-search.component.html',
   styleUrls: ['./book-search.component.scss']
 })
-export class BookSearchComponent implements OnInit {
+export class BookSearchComponent implements OnInit, OnDestroy {
   public books: ReadingListBook[];
   public searchForm: FormGroup;
-  
+  tempBookForUndo : Book;
+  snackbarsubscription: Subscription;
 
   constructor(
     private readonly store: Store,
-    private readonly fb: FormBuilder
+    private readonly fb: FormBuilder,
+    private snackBar: MatSnackBar
   ) {
     this.searchForm = this.fb.group({
       term: ''
@@ -40,6 +45,20 @@ export class BookSearchComponent implements OnInit {
     });
   }
 
+  // open snackbar
+  showSnackbarDuration(content, action) {
+    const snackBarDuration = 1500;
+    const snack = this.snackBar.open(content, action,{
+      duration: snackBarDuration
+    });
+
+    this.snackbarsubscription = snack.onAction().subscribe(() => {
+      const item : ReadingListItem = {...this.tempBookForUndo, bookId : this.tempBookForUndo?.id || '' };
+      this.store.dispatch(removeFromReadingList({item}));
+
+    });
+  }
+
   // format the date of publish
   formatDate(date: void | string) {
     return date
@@ -49,7 +68,9 @@ export class BookSearchComponent implements OnInit {
 
   // dispatch an event to add the book to reading list
   addBookToReadingList(book: Book) {
+    this.tempBookForUndo = book;
     this.store.dispatch(addToReadingList({ book }));
+    this.showSnackbarDuration('Added ' + book.title,'undo');
   }
 
   // function to search the javascript(default) books 
@@ -64,6 +85,12 @@ export class BookSearchComponent implements OnInit {
       this.store.dispatch(searchBooks({ term: this.searchTerm }));
     } else {
       this.store.dispatch(clearSearch());
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.snackbarsubscription) { // unsubscribe the observable
+      this.snackbarsubscription.unsubscribe();
     }
   }
 }
